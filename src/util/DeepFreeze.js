1eba821c87e48e387812cc8c830555f33f24cfe7
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-13 02:39:19 +0900

    refactor: DeepFreeze 모듈 선언 방식 변경

--- src/util/DeepFreeze.js
+++ src/util/DeepFreeze.js
@@ -1,13 +1,13 @@
-function deepFreeze(obj) {
+const DeepFreeze = (obj) => {
   Object.freeze(obj);
 
   Object.keys(obj).forEach((key) => {
     if (typeof obj[key] === 'object' && obj[key] !== null) {
-      deepFreeze(obj[key]);
+      DeepFreeze(obj[key]);
     }
   });
 
   return obj;
 }
 
-export { deepFreeze };
+export default DeepFreeze;
