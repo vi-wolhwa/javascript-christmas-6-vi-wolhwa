00a1282c0dcc4b801b17e2c9b7156a82c15273f4
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-12 20:13:46 +0900

    refactor: EventData Benefits 리팩토링
    discount와 giveaway를 분리

--- src/constant/data/EventData.js
+++ src/constant/data/EventData.js
@@ -17,6 +17,7 @@
  */
 
 import { OPTIONS } from "../Options.js";
+import { MENU_DATA } from "../data/MenuData.js"
 import { deepFreeze } from "../../util/DeepFreeze.js";
 
 const type = OPTIONS.event.type;
@@ -33,39 +34,41 @@ const EVENT_CONDITION = (orderForm) => orderForm.total_price >= 10000;
 const EVENT_DATA = deepFreeze([
   {
     name: '크리스마스 디데이 할인',
-    type: type.discount,
     condition: (orderForm) => orderForm.day <= christmas,
-    benefits: (orderForm) => 1000 + (100 * (orderForm.day - 1)),
+    discount: (orderForm) => 1000 + (100 * (orderForm.day - 1)),
+    giveaway: [],
     description: '크리스마스가 다가올수록 날마다 할인 금액이 1,000원부터 100원씩 증가'
   },
   {
     name: '평일 할인',
-    type: type.discount,
     condition: (orderForm) => weekday.includes(orderForm.day_of_week),
-    benefits: (orderForm) => 2023 * orderForm.count[category.dessert],
+    discount: (orderForm) => 2023 * orderForm.count[category.dessert],
+    giveaway: [],
     description: '평일에는 디저트 메뉴를 메뉴 1개당 2,023원 할인'
   },
   {
     name: '주말 할인',
-    type: type.discount,
     condition: (orderForm) => weekend.includes(orderForm.day_of_week),
-    benefits: (orderForm) => 2023 * orderForm.count[category.main],
+    discount: (orderForm) => 2023 * orderForm.count[category.main],
+    giveaway: [],
     description: '주말에는 메인 메뉴를 메뉴 1개당 2,023원 할인'
   },
   {
     name: '특별 할인',
-    type: type.discount,
     condition: (orderForm) => starDay.includes(orderForm.day),
-    benefits: () => 1000,
+    discount: () => 1000,
+    giveaway: [],
     description: '이벤트 달력에 별이 있으면 총주문 금액에서 1,000원 할인'
   },
   {
     name: '증정 이벤트',
-    type: type.giveaway,
     condition: (orderForm) => orderForm.total_price >= 120000,
-    benefits: () => ['샴페인'],
+    discount: () => 0,
+    giveaway: [{ name: MENU_DATA['샴페인'], count: 1 }],
     description: '할인 전 총주문 금액이 12만 원 이상일 때, 샴페인 1개 증정'
   }
 ]);
+
+console.log(EVENT_DATA[3].giveaway)
 
 export { EVENT_DATA, EVENT_CONDITION };
