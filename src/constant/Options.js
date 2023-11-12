8a981402043016174fbc22a40c4e03cfff3d3c54
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-12 14:57:26 +0900

    feat: Event에 사용되는 Options 추가

--- src/constant/Options.js
+++ src/constant/Options.js
@@ -9,12 +9,29 @@ const OPTIONS = deepFreeze({
       beverage: '음료',
     }
   },
+
+  event: {
+    type: {
+      discount: '할인',
+      giveaway: '증정'
+    },
+
+  },
+
   date: {
     year: 2023,
     month: 12,
     firstDate: 1,
     lastDate: 31,
-    anniversary: { christmas: 25 },
+    daysOfWeek: {
+      0: 'Sunday',
+      1: 'Monday',
+      2: 'Tuesday',
+      3: 'Wednesday',
+      4: 'Thursday',
+      5: 'Friday',
+      6: 'Saturday'
+    }
   }
 });
 
