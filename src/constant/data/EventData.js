f8c58dfe3cd84ba383f39c3bac66a45272697d85
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-12 19:31:25 +0900

    remove: EventForm 삭제 및 EventData 복구
    EventForm의 역할이 불분명하게 되어 삭제

--- src/constant/data/EventData.js
+++ src/constant/data/EventData.js
@@ -17,7 +17,6 @@
  */
 
 import { OPTIONS } from "../Options.js";
-import { EVENT_FORM } from "../form/EventForm.js";
 import { deepFreeze } from "../../util/DeepFreeze.js";
 
 const type = OPTIONS.event.type;
@@ -29,42 +28,44 @@ const starDay = [3, 10, 17, 24, 25, 31];
 const weekday = Object.values(dow).slice(0, 5);
 const weekend = Object.values(dow).slice(5, 7);
 
+const EVENT_CONDITION = (orderForm) => orderForm.total_price >= 10000;
+
 const EVENT_DATA = deepFreeze([
-  EVENT_FORM(
-    '크리스마스 디데이 할인',
-    type.discount,
-    (orderFrom) => orderFrom.day <= christmas,
-    (orderFrom) => 1000 + (100 * (orderFrom.day - 1)),
-    '크리스마스가 다가올수록 날마다 할인 금액이 1,000원부터 100원씩 증가'
-  ),
-  EVENT_FORM(
-    '평일 할인',
-    type.discount,
-    (orderFrom) => weekday.includes(orderFrom.day_of_week),
-    (orderFrom) => 2023 * orderFrom.count[category.dessert],
-    '평일에는 디저트 메뉴를 메뉴 1개당 2,023원 할인'
-  ),
-  EVENT_FORM(
-    '주말 할인',
-    type.discount,
-    (orderFrom) => weekend.includes(orderFrom.day_of_week),
-    (orderFrom) => 2023 * orderFrom.count[category.main],
-    '주말에는 메인 메뉴를 메뉴 1개당 2,023원 할인'
-  ),
-  EVENT_FORM(
-    '특별 할인',
-    type.discount,
-    (orderFrom) => starDay.includes(orderFrom.day),
-    () => 1000,
-    '이벤트 달력에 별이 있으면 총주문 금액에서 1,000원 할인'
-  ),
-  EVENT_FORM(
-    '증정 이벤트',
-    type.giveaway,
-    (orderFrom) => orderFrom.total_price >= 120000,
-    () => ['샴페인'],
-    '할인 전 총주문 금액이 12만 원 이상일 때, 샴페인 1개 증정'
-  ),
+  {
+    name: '크리스마스 디데이 할인',
+    type: type.discount,
+    condition: (orderForm) => orderForm.day <= christmas,
+    benefits: (orderForm) => 1000 + (100 * (orderForm.day - 1)),
+    description: '크리스마스가 다가올수록 날마다 할인 금액이 1,000원부터 100원씩 증가'
+  },
+  {
+    name: '평일 할인',
+    type: type.discount,
+    condition: (orderForm) => weekday.includes(orderForm.day_of_week),
+    benefits: (orderForm) => 2023 * orderForm.count[category.dessert],
+    description: '평일에는 디저트 메뉴를 메뉴 1개당 2,023원 할인'
+  },
+  {
+    name: '주말 할인',
+    type: type.discount,
+    condition: (orderForm) => weekend.includes(orderForm.day_of_week),
+    benefits: (orderForm) => 2023 * orderForm.count[category.main],
+    description: '주말에는 메인 메뉴를 메뉴 1개당 2,023원 할인'
+  },
+  {
+    name: '특별 할인',
+    type: type.discount,
+    condition: (orderForm) => starDay.includes(orderForm.day),
+    benefits: () => 1000,
+    description: '이벤트 달력에 별이 있으면 총주문 금액에서 1,000원 할인'
+  },
+  {
+    name: '증정 이벤트',
+    type: type.giveaway,
+    condition: (orderForm) => orderForm.total_price >= 120000,
+    benefits: () => ['샴페인'],
+    description: '할인 전 총주문 금액이 12만 원 이상일 때, 샴페인 1개 증정'
+  }
 ]);
 
-export { EVENT_DATA };
+export { EVENT_DATA, EVENT_CONDITION };
