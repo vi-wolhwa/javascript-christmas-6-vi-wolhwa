9f5a2694e053a3fa623010a919b4da58e2a33a5b
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-12 18:08:41 +0900

    refactor: EventData 변경에 따른 Test 변경

--- __tests__/constant/EventData.test.js
+++ __tests__/constant/EventData.test.js
@@ -1,71 +1,88 @@
 import { EVENT_DATA } from "../../src/constant/EventData.js";
+import { ORDER_FORM } from "../../src/constant/OrderForm.js";
 import { MENU_DATA } from "../../src/constant/MenuData.js";
 
 describe("EVENT_DATA 객체 테스트", () => {
+  let orderForm;
+
+  beforeEach(() => {
+    orderForm = ORDER_FORM();
+  });
   test("크리스마스 디데이 할인", () => {
-    const christmasEvent = EVENT_DATA[0];
-    const christmas = 25
+    const event = EVENT_DATA[0];
 
     // 이벤트 조건 경계값 테스트
-    expect(christmasEvent.condition(christmas)).toBeTruthy();
-    expect(christmasEvent.condition(christmas + 1)).toBeFalsy();
+    orderForm.day = 25;
+    expect(event.condition(orderForm)).toBeTruthy();
+    orderForm.day = 26;
+    expect(event.condition(orderForm)).toBeFalsy();
 
     // 할인 금액 확인
-    expect(christmasEvent.benefits(christmas)).toBe(3400);
+    orderForm.day = 25;
+    expect(event.benefits(orderForm)).toBe(3400);
   });
 
   test("평일 할인", () => {
-    const weekdayDiscountEvent = EVENT_DATA[1];
+    const event = EVENT_DATA[1];
     const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday'];
     const weekend = ['Friday', 'Saturday'];
 
     // 이벤트 조건 테스트 (평일)
-    weekday.forEach((day) => {
-      expect(weekdayDiscountEvent.condition(day)).toBeTruthy();
+    weekday.forEach((dayOfWeek) => {
+      orderForm.day_of_week = dayOfWeek;
+      expect(event.condition(orderForm)).toBeTruthy();
     });
 
     // 이벤트 조건 테스트 (주말)
-    weekend.forEach((day) => {
-      expect(weekdayDiscountEvent.condition(day)).toBeFalsy();
+    weekend.forEach((dayOfWeek) => {
+      orderForm.day_of_week = dayOfWeek;
+      expect(event.condition(orderForm)).toBeFalsy();
     });
   });
 
   test("주말 할인", () => {
-    const weekendDiscountEvent = EVENT_DATA[2];
+    const event = EVENT_DATA[2];
     const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday'];
     const weekend = ['Friday', 'Saturday'];
 
     // 이벤트 조건 테스트 (주말)
-    weekend.forEach((day) => {
-      expect(weekendDiscountEvent.condition(day)).toBeTruthy();
+    weekend.forEach((dayOfWeek) => {
+      orderForm.day_of_week = dayOfWeek;
+      expect(event.condition(orderForm)).toBeTruthy();
     });
 
     // 이벤트 조건 테스트 (평일)
-    weekday.forEach((day) => {
-      expect(weekendDiscountEvent.condition(day)).toBeFalsy();
+    weekday.forEach((dayOfWeek) => {
+      orderForm.day_of_week = dayOfWeek;
+      expect(event.condition(orderForm)).toBeFalsy();
     });
   });
 
   test("특별 할인", () => {
-    const specialDiscountEvent = EVENT_DATA[3];
+    const event = EVENT_DATA[3];
     const starDay = [3, 10, 17, 24, 25, 31];
 
     // 이벤트 조건 테스트
     starDay.forEach((day) => {
-      expect(specialDiscountEvent.condition(day)).toBeTruthy();
-      expect(specialDiscountEvent.condition(day + 2)).toBeFalsy();
+      orderForm.day = day;
+      expect(event.condition(orderForm)).toBeTruthy();
     });
+
+    orderForm.day = 26;
+    expect(event.condition(orderForm)).toBeFalsy();
   });
 
   test("증정 이벤트", () => {
-    const giveawayEvent = EVENT_DATA[4];
+    const event = EVENT_DATA[4];
 
     // 이벤트 조건 경계값 테스트
-    expect(giveawayEvent.condition(120000)).toBeTruthy();
-    expect(giveawayEvent.condition(119999)).toBeFalsy();
+    orderForm.total_price = 120000;
+    expect(event.condition(orderForm)).toBeTruthy();
+    orderForm.total_price = 119999;
+    expect(event.condition(orderForm)).toBeFalsy();
 
     // 증정 메뉴 확인
-    giveawayEvent.benefits().forEach((giveaway) => {
+    event.benefits().forEach((giveaway) => {
       expect(Object.keys(MENU_DATA)).toContain(giveaway);
     });
   });
