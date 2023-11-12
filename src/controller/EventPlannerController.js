c743d67bb951fb17ed08e0e7c86f2dfdfb660250
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-13 00:54:07 +0900

    feat: EventPlannerController 템플릿 생성

---
+++ src/controller/EventPlannerController.js
@@ -0,0 +1,70 @@
+class EventPlannerController {
+
+  run() {
+    this.#displayIntroduce();
+    this.#handleUserInput();
+    this.#displayResultHeader();
+    this.#displayResultInOrder();
+  }
+
+  #displayIntroduce() {
+
+  }
+
+  #handleUserInput() {
+    this.#handleDateInput();
+    this.#handleOrderInput();
+  }
+
+  #displayResultHeader() {
+
+  }
+
+  #displayResultInOrder() {
+    this.#displayOrderMenus();
+    this.#displayTotalPrice();
+    this.#displayGiveaways();
+    this.#displayBenefitDetails();
+    this.#displayTotalBenefitAmount();
+    this.#displayDiscountedPrice();
+    this.#displayEventBadge();
+  }
+
+  #handleDateInput() {
+
+  }
+
+  #handleOrderInput() {
+
+  }
+
+  #displayOrderMenus() {
+
+  }
+
+  #displayTotalPrice() {
+
+  }
+
+  #displayGiveaways() {
+
+  }
+
+  #displayBenefitDetails() {
+
+  }
+
+  #displayTotalBenefitAmount() {
+
+  }
+
+  #displayDiscountedPrice() {
+
+  }
+
+  #displayEventBadge() {
+
+  }
+}
+
+export default EventPlannerController;
