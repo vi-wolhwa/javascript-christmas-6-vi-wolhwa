2ed3b54258e6fbc595d7b71167d3e5e0816ef224
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-12 19:30:29 +0900

    refactor: EventData.test.js import 경로 fix

--- __tests__/util/data/EventData.test.js
+++ __tests__/util/data/EventData.test.js
@@ -1,6 +1,6 @@
-import { EVENT_DATA } from "../../src/constant/data/EventData.js";
-import { ORDER_FORM } from "../../src/constant/form/OrderForm.js";
-import { MENU_DATA } from "../../src/constant/data/MenuData.js";
+import { EVENT_DATA } from "../../../src/constant/data/EventData.js";
+import { ORDER_FORM } from "../../../src/constant/form/OrderForm";
+import { MENU_DATA } from "../../../src/constant/data/MenuData.js";
 
 describe("EVENT_DATA 객체 테스트", () => {
   let orderForm;
