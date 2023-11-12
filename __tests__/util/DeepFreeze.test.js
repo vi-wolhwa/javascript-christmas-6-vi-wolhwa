49f1aefdb1093db818d8c35fd3507a148d813efb
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-12 15:17:29 +0900

    style: DeepFreeze.js 주석 컨벤션 적용
    주석 위에 공백 라인 확보

--- __tests__/util/DeepFreeze.test.js
+++ __tests__/util/DeepFreeze.test.js
@@ -17,6 +17,7 @@ describe('deepFreeze 함수 테스트', () => {
   });
 
   test('객체를 재귀적으로 동결하는지 확인', () => {
+
     // 첫 번째 단계 동결 확인
     expect(Object.isFrozen(obj)).toBe(true);
 
@@ -26,6 +27,7 @@ describe('deepFreeze 함수 테스트', () => {
   });
 
   test('동결된 객체 속성 변경 방지', () => {
+
     // 속성 변경 시도
     expect(() => (obj.prop1 = 'new value')).toThrow();
     expect(() => obj.prop2 = 'new value').toThrow();
