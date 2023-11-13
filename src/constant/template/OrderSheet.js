c8892fed005bb6866edb414c70a47ad97de273c6
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-13 21:54:49 +0900

    feat: OrderSheet(주문정보) 객체

---
+++ src/constant/template/OrderSheet.js
@@ -0,0 +1,24 @@
+const OrderSheet = {
+	keys: {
+		date: 'date',
+		day_of_week: 'day_of_week',
+		order: 'order',
+		total_price: 'total_price',
+		available_events: 'available_events',
+		giveaways: 'giveaways',
+		total_discount: 'total_discount',
+		total_benefits: 'total_benefits',
+		discounted_price: 'discounted_price'
+	},
+	date: null,
+	day_of_week: null,
+	order: [],
+	total_price: null,
+	available_events: [],
+	giveaways: [],
+	total_discount: null,
+	total_benefits: null,
+	discounted_price: null
+};
+
+export default OrderSheet;
