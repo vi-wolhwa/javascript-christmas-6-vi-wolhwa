f07d12d98e5609b9959d1ab1d85a787d7fadb83c
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-13 12:44:35 +0900

    feat: Validator 구현, 유효성검사 수행

--- src/validation/EventPlannerValidator.js
+++ src/validation/EventPlannerValidator.js
@@ -1,5 +1,7 @@
 import DateValidation from './eventPlannerValidation/DateValidation.js';
-import DeepFreeze from '../../util/DeepFreeze.js';
+import MenuNameValidation from './eventPlannerValidation/MenuNameValidation.js';
+import MenuCountValidation from './eventPlannerValidation/MenuCountValidation.js';
+import DeepFreeze from '../util/DeepFreeze.js';
 
 /** EventPlanner 내에서 유효성검사를 담당하는 Validator */
 const EventPlannerValidator = DeepFreeze({
@@ -9,6 +11,15 @@ const EventPlannerValidator = DeepFreeze({
 	 */
 	validateDate(date) {
 		DateValidation.validate(date);
+	},
+
+	/**
+	 * 주문(UserInput)에 대하여 유효성검사를 수행한다.
+	 * @param {Array<object>} orders 주문
+	 */
+	validateOrders(orders) {
+		MenuNameValidation.validate(orders.map((order) => order.name));
+		MenuCountValidation.validate(orders.map((order) => order.count));
 	}
 });
 
