27bb995214b99f3adc32453049e0cc4344b92cb4
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-13 22:44:07 +0900

    feat: Order> OrderSheet를 복사하는 방식으로 변경

--- src/domain/Order.js
+++ src/domain/Order.js
@@ -1,11 +1,9 @@
-class Order {
-	#date;
-	#orders;
+import { OrderSheet, OrderSheetKeys as Keys } from '../constant/template/OrderSheet.js';
+import DeepCopy from './../util/DeepCopy.js';
 
-	constructor(date, orders) {
-		this.#date = date;
-		this.#orders = orders;
-	}
+class Order {
+	/** @type {OrderSheet} */
+	#orderSheet = DeepCopy(OrderSheet);
 }
 
 export default Order;
