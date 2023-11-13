9314e2a1f6471a9f2cbe820790724321253711d6
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-13 12:45:45 +0900

    comment: validation 개발 코멘트 작성

--- src/validation/eventPlannerValidation/DateValidation.js
+++ src/validation/eventPlannerValidation/DateValidation.js
@@ -1,3 +1,10 @@
+/**
+ * [동일한 에러 발생 코드 반복]
+ * 지금은 모두 동일한 메시지를 출력하지만,
+ * 에러메시지를 구별해야 하는 경우를 고려하여 반복 작성하였다.
+ * (MenuCountValidation.js, MenuNameValidation.js 동일)
+ */
+
 import ERROR_MESSAGES from '../../constant/strings/ErrorMessages.js';
 import OPTIONS from './../../constant/Options.js';
 import DeepFreeze from '../../util/DeepFreeze.js';
