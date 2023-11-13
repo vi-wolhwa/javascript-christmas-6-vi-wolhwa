d1fb4ca28f7b748723cdd3f8347322c99d466ac7
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-13 14:23:15 +0900

    comment: Controller 구현 함수 주석 작성

--- src/controller/EventPlannerController.js
+++ src/controller/EventPlannerController.js
@@ -10,7 +10,7 @@ class EventPlannerController {
 	/** @type {Order} */
 	#order;
 
-	/** 이벤트 플래너 프로그램을 실행한다. */
+	// 이벤트 플래너 프로그램을 실행한다.
 	async run() {
 		this.#displayIntroduce();
 		await this.#handleUserInput();
@@ -18,31 +18,50 @@ class EventPlannerController {
 		this.#displayResultInOrder();
 	}
 
-	/** Introduce 메시지를 출력한다. */
+	// Introduce 메시지를 출력한다.
 	#displayIntroduce() {
 		OutputView.printIntroduce();
 	}
 
+	// 사용자입력에 대한 처리를 담당한다.
 	async #handleUserInput() {
 		const date = await this.#handleDateInput();
 		const orders = await this.#handleOrdersInput();
 		this.#order = new Order(date, orders);
 	}
 
+	/**
+	 * 방문날짜(Date)에 대한 처리를 담당한다.
+	 * @returns {number} 방문 날짜
+	 */
 	async #handleDateInput() {
 		return this.#preprocessDate(InputView.readDate());
 	}
 
+	/**
+	 * 주문메뉴(Orders)에 대한 처리를 담당한다.
+	 * @returns {Array<object>} 주문 메뉴 목록 {메뉴명, 개수}
+	 */
 	async #handleOrdersInput() {
 		return this.#preprocessOrder(InputView.readOrder());
 	}
 
+	/**
+	 * 방문날짜(Date)에 대한 전처리를 수행한다.
+	 * @param {string} rawDate 방문날짜(UserInput)
+	 * @returns {number} 방문 날짜
+	 */
 	#preprocessDate(rawDate) {
 		const newDate = rawDate.trim();
 		Validator.validateDate(rawDate);
 		return parseInt(newDate, 10);
 	}
 
+	/**
+	 * 주문메뉴(Orders)에 대한 전처리를 수행한다.
+	 * @param {string} rawOrders 주문(UserInput)
+	 * @returns {Array<object>} 주문 메뉴 목록 {메뉴명, 개수}
+	 */
 	#preprocessOrder(rawOrders) {
 		const newOrders = rawOrders
 			.split(SIGNS.comma)
