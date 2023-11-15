210b20d52df5aec95c76fbe51a51062c37864feb
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-15 21:31:12 +0900

    feat: OrderSheetKeys> 동등성 검사로 무결성 보장

--- src/constant/template/OrderSheet.js
+++ src/constant/template/OrderSheet.js
@@ -2,8 +2,11 @@
  * Q. Key 객체를 선언한 이유
  *   : 개발 편의성을 위하여 선언하였다.
  *     객체를 Key로 참조할 때, Key 이름에 오타가 발생하는 경우를 예방하였다.
+ *     DeepEqualityCheck 모듈의 동등성 검사를 통해 무결성을 보장한다.
  *     ex) Order.js > Order > updateOrderSheet({ key, value })
  */
+
+import DeepEqualityCheck from '../../util/DeepEqualityCheck.js';
 
 const ORDER_SHEET_KEYS = {
 	visitDay: 'visitDay',
@@ -30,5 +33,8 @@ const ORDER_SHEET = {
 	discounted_price: null,
 	event_badge: null
 };
+
+DeepEqualityCheck(Object.keys(ORDER_SHEET_KEYS), Object.values(ORDER_SHEET_KEYS));
+DeepEqualityCheck(Object.keys(ORDER_SHEET_KEYS), Object.keys(ORDER_SHEET));
 
 export { ORDER_SHEET, ORDER_SHEET_KEYS };
