62bf47fe5e4e8fc25a9f112310715f072ccb6a7f
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-11-15 19:51:10 +0900

    comment: OrderSheet> 개발 코멘트 작성

--- src/constant/template/OrderSheet.js
+++ src/constant/template/OrderSheet.js
@@ -1,3 +1,10 @@
+/**
+ * Q. Key 객체를 선언한 이유
+ *   : 개발 편의성을 위하여 선언하였다.
+ *     객체를 Key로 참조할 때, Key 이름에 오타가 발생하는 경우를 예방하였다.
+ *     ex) Order.js > Order > updateOrderSheet({ key, value })
+ */
+
 const ORDER_SHEET_KEYS = {
 	visitDay: 'visitDay',
 	day_of_week: 'day_of_week',
